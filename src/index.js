6e09eeeb07e119a61f5360b4cad10f1d9802746f
ubm <Mikalai.Yaroshka@solbeg.com>
2024-10-20 19:39:29 +0300

    update: done the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let result = [];
+    const startsWithOne = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundreds = ['','one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+    let numberArr = String(number).split("");
+
+    if(number === 0) return 'zero';
+
+    if(numberArr.length === 3) {
+        result.push(hundreds[+numberArr[0]]);
+        numberArr = numberArr.slice(1);
+    }
+
+    if(+numberArr.join('') < 20) {
+        result.push(startsWithOne[+numberArr.join('')]);
+    } else {
+        result.push(tens[+numberArr[0]]);
+        result.push(startsWithOne[+numberArr[1]]);
+    }
+
+    
+
+    console.log(result.join(" "));
+    return result.join(" ").trim();
+};
+
